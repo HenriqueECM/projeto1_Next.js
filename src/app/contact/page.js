111f28723c4c8cc0fdfa46eacd28db8d4e90457c
henriqueworks68 <henriqueworks68@gmail.com>
2025-06-25 16:58:31 -0300

    1.5 - colocando conteudo na pagina contato e ajustando css

--- src/app/contact/page.js
+++ src/app/contact/page.js
@@ -1,9 +1,12 @@
-import Header from '../../components/Header';
 import "../../styles/globals.css";
 
 export default function Contact() {
     return (
         <div>
+            <section>
+                <h1 className="text-4xl">Contato</h1>
+                <p className="pt-4">email: henrique_menel@estudante.sesisenai.org.br</p>
+            </section>
         </div>
     )
 }
--- src/app/page.js
+++ src/app/page.js
@@ -5,8 +5,8 @@ export default function Home() {
     <div>
       <section>
         <h1 className="text-4xl">Olá, Eu sou Henrique Menel</h1>
-        <h2 className="text-2xl">Sobre mim</h2>
-        <p className="">Olá! Me chamo Henrique Menel e atualmente sou aprendiz de Desenvolvimento de Sistemas no CentroWeg. Estou em constante evolução como desenvolvedor, aprendendo e aplicando conhecimentos tanto de back-end quanto de front-end, com foco principal em Java e JavaScript.
+        <h2 className="text-2xl pt-3">Sobre mim</h2>
+        <p className="pt-2">Olá! Me chamo Henrique Menel e atualmente sou aprendiz de Desenvolvimento de Sistemas no CentroWeg. Estou em constante evolução como desenvolvedor, aprendendo e aplicando conhecimentos tanto de back-end quanto de front-end, com foco principal em Java e JavaScript.
           Sou movido por desafios e tenho um forte compromisso com a dedicação, persistência e aprendizado contínuo. Acredito que crescer na área da tecnologia exige acompanhar seu ritmo acelerado — e é exatamente isso que busco: evoluir todos os dias.
           Estou sempre aberto a colaborações, especialmente em projetos que me ajudem a expandir meus conhecimentos e ganhar mais experiência prática como desenvolvedor. Se quiser trocar ideias ou construir algo juntos, será um prazer conversar!</p>
       </section>
