d5a97e2fb6ccdb0e2d7d46b0fb3391d1a272ed3b
henriqueworks68 <henriqueworks68@gmail.com>
2025-06-25 16:55:01 -0300

    1.5 - adicionado conteudo dentro da pagina sobre

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -4,6 +4,12 @@ import "../../styles/globals.css";
 export default function About() {
     return (
         <div>
+            <section>
+                <h1 className="text-4xl">Sobre mim</h1>
+                <p className="">Olá! Me chamo Henrique Menel e atualmente sou aprendiz de Desenvolvimento de Sistemas no CentroWeg. Estou em constante evolução como desenvolvedor, aprendendo e aplicando conhecimentos tanto de back-end quanto de front-end, com foco principal em Java e JavaScript.
+                    Sou movido por desafios e tenho um forte compromisso com a dedicação, persistência e aprendizado contínuo. Acredito que crescer na área da tecnologia exige acompanhar seu ritmo acelerado — e é exatamente isso que busco: evoluir todos os dias.
+                    Estou sempre aberto a colaborações, especialmente em projetos que me ajudem a expandir meus conhecimentos e ganhar mais experiência prática como desenvolvedor. Se quiser trocar ideias ou construir algo juntos, será um prazer conversar!</p>
+            </section>
         </div>
     );
 }
