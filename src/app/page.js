64679cf8b9ba1045abf4ffc80d10a18f70d0204a
henrique <henriqueworks68@gmail.com>
2025-06-26 19:51:11 -0300

    1.6

--- src/app/page.js
+++ src/app/page.js
@@ -2,13 +2,13 @@ import "../styles/globals.css";
 
 export default function Home() {
   return (
-    <div>
+    <div className="bg-white dark:bg-black text-black dark:text-white transition-colors duration-300 min-h-screen p-10">
       <section>
-        <h1 className="text-4xl">Olá, Eu sou Henrique Menel</h1>
+        <h1 className="text-4xl font-bold">Olá, Eu sou Henrique Menel</h1>
         <h2 className="text-2xl pt-3">Sobre mim</h2>
-        <p className="pt-2">Olá! Me chamo Henrique Menel e atualmente sou aprendiz de Desenvolvimento de Sistemas no CentroWeg. Estou em constante evolução como desenvolvedor, aprendendo e aplicando conhecimentos tanto de back-end quanto de front-end, com foco principal em Java e JavaScript.
-          Sou movido por desafios e tenho um forte compromisso com a dedicação, persistência e aprendizado contínuo. Acredito que crescer na área da tecnologia exige acompanhar seu ritmo acelerado — e é exatamente isso que busco: evoluir todos os dias.
-          Estou sempre aberto a colaborações, especialmente em projetos que me ajudem a expandir meus conhecimentos e ganhar mais experiência prática como desenvolvedor. Se quiser trocar ideias ou construir algo juntos, será um prazer conversar!</p>
+        <p className="pt-2">
+          Olá! Me chamo Henrique Menel e atualmente sou aprendiz de Desenvolvimento de Sistemas no CentroWeg...
+        </p>
       </section>
     </div>
   );
