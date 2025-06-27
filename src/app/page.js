7ea3a6e7550db4c73b2e7d711f751a46438a6ab2
henrique <henriqueworks68@gmail.com>
2025-06-26 21:07:38 -0300

    1.6.1 tentando colocar button dark

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,6 @@
-import "../styles/globals.css";
-
 export default function Home() {
   return (
-    <div className="bg-white dark:bg-black text-black dark:text-white transition-colors duration-300 min-h-screen p-10">
+    <div className="min-h-screen p-10 transition-colors duration-300">
       <section>
         <h1 className="text-4xl font-bold">Olá, Eu sou Henrique Menel</h1>
         <h2 className="text-2xl pt-3">Sobre mim</h2>
